7745f45e42a49c0a4dd4df31b786803279fc1715
Shaurya Singh <honourableshaurya@gmail.com>
2026-02-02 22:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (18.20%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,23 @@
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize == 0) return 0;
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        // Update minimum price so far
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        // Calculate profit if sold today
+        int profit = prices[i] - minPrice;
+
+        // Update maximum profit
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
