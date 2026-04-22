b2998361e746b6011183d3364b32807ea796af04
Shaurya Singh <honourableshaurya@gmail.com>
2026-04-22 12:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (24.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.c
+++ 801-is-graph-bipartite/is-graph-bipartite.c
@@ -2,14 +2,15 @@
 
 bool isBipartite(int** graph, int graphSize, int* graphColSize) {
 
-    int color[graphSize];
+    int color[100];  // since n <= 100
 
-    // initialize colors
+    // initialize
     for(int i = 0; i < graphSize; i++)
         color[i] = -1;
 
-    int queue[graphSize];
+    int queue[100];
 
+    // handle disconnected graph
     for(int i = 0; i < graphSize; i++) {
 
         if(color[i] == -1) {
@@ -25,10 +26,12 @@ bool isBipartite(int** graph, int graphSize, int* graphColSize) {
                 for(int j = 0; j < graphColSize[node]; j++) {
                     int neighbor = graph[node][j];
 
+                    // not colored
                     if(color[neighbor] == -1) {
                         color[neighbor] = 1 - color[node];
                         queue[rear++] = neighbor;
                     }
+                    // same color → conflict
                     else if(color[neighbor] == color[node]) {
                         return false;
                     }
